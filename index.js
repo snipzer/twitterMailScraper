153317e5bf97893c70c01db809f69c16136385ab
JeanPoulard <jean.poulardbore@gmail.com>
2017-04-24 10:55:12 +0200

    modif index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const config = require('config-yml');
 
-console.log(config.toto.test);
+console.log(default.app.twitter.accessToken);
+console.log(default.app.twitter.accessTokenSecret);
+console.log(default.app.twitter.consumerKey);
+console.log(default.app.twitter.consumerSecret);
