9b94a83bf68d851bde72bd6c7a53a5530774509f
snipzer <l.arif@laposte.net>
2017-04-24 11:38:20 +0200

    Ajout de babel

--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
-const config = require('config-yml');
-
+//const config = require('config-yml');
+import config from 'config-yml';
 console.log(config.default.app.twitter.accessToken);
 console.log(config.default.app.twitter.accessTokenSecret);
 console.log(config.default.app.twitter.consumerKey);
