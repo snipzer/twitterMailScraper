fe091ad1ec58c2851b2680b0322361b0e63f9550
snipzer <l.arif@laposte.net>
2017-04-24 14:57:52 +0200

    paramètre pour l'index

--- build/index.js
+++ build/index.js
@@ -8,11 +8,31 @@ var _fs = require('fs');
 
 var _fs2 = _interopRequireDefault(_fs);
 
+var _argparse = require('argparse');
+
+var _argparse2 = _interopRequireDefault(_argparse);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var config = _jsYaml2.default.safeLoad(_fs2.default.readFileSync('config/config.yml', 'utf8'));
 
-console.log(config.default.app.twitter.accessToken);
-console.log(config.default.app.twitter.accessTokenSecret);
-console.log(config.default.app.twitter.consumerKey);
-console.log(config.default.app.twitter.consumerSecret);
+// console.log(config.default.app.twitter.accessToken);
+// console.log(config.default.app.twitter.accessTokenSecret);
+// console.log(config.default.app.twitter.consumerKey);
+// console.log(config.default.app.twitter.consumerSecret);
+
+var ArgParse = _argparse2.default.ArgumentParser;
+
+var parser = new ArgParse({
+    version: '0.0.1',
+    addHelp: true,
+    description: 'Used for create the request to twitter'
+});
+
+parser.addArgument(['-k', '--keyword'], {
+    help: "The keyword to search for"
+});
+
+var args = parser.parseArgs();
+
+console.log(args.keyword);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
+import ArgParseObj from 'argparse';
 
 
 const config = yaml.safeLoad(fs.readFileSync('config/config.yml', 'utf8'));
 
-console.log(config.default.app.twitter.accessToken);
-console.log(config.default.app.twitter.accessTokenSecret);
-console.log(config.default.app.twitter.consumerKey);
-console.log(config.default.app.twitter.consumerSecret);
+// console.log(config.default.app.twitter.accessToken);
+// console.log(config.default.app.twitter.accessTokenSecret);
+// console.log(config.default.app.twitter.consumerKey);
+// console.log(config.default.app.twitter.consumerSecret);
+
+const ArgParse = ArgParseObj.ArgumentParser;
+
+const parser = new ArgParse({
+    version: '0.0.1',
+    addHelp:true,
+    description: 'Used for create the request to twitter'
+});
+
+parser.addArgument(
+    [ '-k', '--keyword' ],
+    {
+        help: "The keyword to search for"
+    }
+);
+
+const args = parser.parseArgs();
+
+
+console.log(args.keyword);
